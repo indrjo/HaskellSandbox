a4296d86f29df12ae82e495efea671d76deb7db3
indrjo <dedej.indrjo@gmail.com>
2020-11-28 21:53:33 +0100

    Add files via upload

--- kalamares.hs
+++ kalamares.hs
@@ -1,5 +1,9 @@
 
-import Control.Monad       (liftM, forM_)
+{-
+  *** Description yet to come... ***
+-}
+
+import Control.Monad       (liftM)
 import Control.Conditional (ifM)
 import Data.List.Extra     (trim)
 import System.Environment  (getArgs)
@@ -8,71 +12,66 @@ import System.Directory    (doesFileExist)
 import System.Process      (callCommand)
 
 main :: IO ()
-main = do
-  files <- getArgs
-  if null files
-    then say "provide some file to parse! doing nothing..."
-    else forM_ files $ \f -> ifM (doesFileExist f)
-        (kalamares f)
-        --(do { say $ "reading \'" ++ f ++ "\'..."; kalamares f })
-        (say $ "\'" ++ f ++ "\' does not exist!")
+main = mapM_ kalamares =<< getArgs
 
 -- kalamares data
-data Kalamares = Base FilePath FilePath
-               | Copy FilePath FilePath
-               | Unknown String
+data Kalamares = NotToParse
+               | FT FilePath FilePath
+               | CP FilePath FilePath
+               | MV FilePath FilePath
+               | IDK String
 
 -- turn a string into a kalamares data
-toKal :: String -> Kalamares
-toKal str
-    | elem '@' str = let (p1, p2) = chop '@' str in Base p1 p2
-    | elem '>' str = let (p1, p2) = chop '>' str in Copy p1 p2
-    | otherwise    = Unknown str
+kal :: String -> Kalamares
+kal str
+    | isNotToParse str = NotToParse
+    | elem '&' str     = let (p1, p2) = chop '&' str in FT p1 p2
+    | elem '>' str     = let (p1, p2) = chop '>' str in CP p1 p2
+    | elem '@' str     = let (p1, p2) = chop '@' str in MV p1 p2
+    | otherwise        = IDK (trim str)
   where
+    isNotToParse :: String -> Bool
+    isNotToParse str = case dropWhile (== ' ') str of
+        (x:_) -> if x == '#' then True else False
+        []    -> True
     chop :: Char -> String -> (String, String)
     chop c str = (trim a, trim . drop 1 $ b)
-      where (a, b) = span (/= c) str
+      where
+        (a, b) = span (/= c) . takeWhile (/= '#') $ str
 
 -- *** core functions ***
 -- "kalamares" is the core function: takes a file and parses it.
 kalamares :: FilePath -> IO ()
-kalamares f = helper "" "" =<< getMeaningfulLinesOf f
+kalamares f = ifM (doesFileExist f)
+    (kalamaresH =<< liftM lines (readFile f))
+    (say $ "\'" ++ f ++ "\' does not exist!")
   where
     -- This helper function is the actual parser. Takes a line at time, blows 
     -- it up with the "toKal" function and decide what to do.
-    helper :: FilePath -> FilePath -> [String] -> IO ()
-    helper _ _ []     = return ()
-    helper p q (x:xs) = case toKal x of
-        Base a b  -> helper a b xs
-        Copy a b  -> do
-            copy (p </> a) (q </> b)
-            helper p q xs
-        Unknown c -> do
-            say $ "[ " ++ f ++ " ] \'" ++ c ++
-                  "\': what do you want me to do here?"
-            helper p q xs  
-    -- Get all meaningful lines from a file. Here, "meaningful" stands for
-    -- "neither blank line nor line which starts with '#'".
-    getMeaningfulLinesOf :: FilePath -> IO [String]
-    getMeaningfulLinesOf = liftM (filter isMeaningful . lines) . readFile
+    kalamaresH :: [String] -> IO ()
+    kalamaresH = helper "" "" 1
       where
-        isMeaningful :: String -> Bool
-        isMeaningful ""        = False
-        isMeaningful ('#':_ )  = False
-        isMeaningful (' ':str) = isMeaningful str
-        isMeaningful _         = True
+        helper :: FilePath -> FilePath -> Int -> [String] -> IO ()
+        helper _ _ _ []     = return ()
+        helper p q n (x:xs) = case kal x of
+            NotToParse -> helper p q (n+1) xs
+            FT p' q'   -> helper p' q' (n+1) xs
+            CP a b     -> do { cp (p </> a) (q </> b); helper p q (n+1) xs }
+            MV a b     -> do { mv (p </> a) (q </> b); helper p q (n+1) xs }
+            IDK c      -> say $ "[" ++ f ++ ", " ++ show n  ++ "] \'" ++ c ++
+                                "\': what do you expect me to do with that?"
 
 -- *** comunicate with the outside world ***
 say :: String -> IO ()
 say = putStrLn . (" *** " ++)
 
 -- *** functions to re-implement ***
-copy :: FilePath -> FilePath -> IO ()
-copy a b = do
+cp :: FilePath -> FilePath -> IO ()
+cp a b = do
     callCommand $ "mkdir -p " ++ b
     callCommand $ "cp -ruv " ++ a ++ " " ++ b
 
 -- *** function yet to implement ***
-move :: FilePath -> FilePath -> IO ()
-move = undefined
+mv :: FilePath -> FilePath -> IO ()
+mv a b = callCommand $ "mv " ++ a ++ " " ++ b
 
